7e6d20c83fb763c7b3d602e952a2c330859ca096
maxrinal <rinaldimaximilianon@gmail.com>
2024-07-22 22:50:56 -0300

    adding httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HttpClient } from '@angular/common/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, JsonPipe],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit{
-  title = 'Reading a json from assets folder';
-  data :any ;
-  ngOnInit(): void {
-    fetch('./data.json').then(res => res.json())
-    .then(resJson => this.data =JSON.stringify(resJson))
-  }
+export class AppComponent implements OnInit {
+	title = 'Reading a json from assets folder';
+	data: any;
+	constructor(private http: HttpClient) {
+		// http.get<any>('./data.json').subscribe((res:any) => (this.data = res) );
+		// http.get<any>('./data.json').subscribe((res: any) => ((this.data = res), console.log(res)));
+		http.get<any>('/data.json').subscribe((res: any) =>   (this.data = res, console.log('constructor:',res)));
+	}
+	// async ngOnInit(): Promise<any> {
+	ngOnInit(): void {
+		console.log('Ng Init Started');
+		console.log('Im entering here', this.data);
+	}
 }
